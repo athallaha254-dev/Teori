8fbde85b1dced4f62ff1735c0964c327ba3ef425
Athallah Sheva MR <talahtalag@gmail.com>
2025-11-12 10:10:01 +0700

    first commit

---
+++ app/Filament/Resources/KomentarResource/Pages/ListKomentars.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\KomentarResource\Pages;
+
+use App\Filament\Resources\KomentarResource;
+use Filament\Resources\Pages\ListRecords;
+
+class ListKomentars extends ListRecords
+{
+    protected static string $resource = KomentarResource::class;
+}
